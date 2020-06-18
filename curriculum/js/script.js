7e46fb0c3dc62bae8100505e84a9dcdbf00f0376
Your Name <13169110727163@example.com>
2020-06-18 15:00:36 +0800

    0618

--- curriculum/js/script.js
+++ curriculum/js/script.js
@@ -29,6 +29,88 @@ jsonTh.forEach((item)=>{
 })
 tableHtml+=`</tr></thead>`
 
-tableDom.innerHTML = tableHtml ;//元素填内容
 
 
+let tbodyHtml=`<tbody>`;
+let trHtml=``;
+
+for(let i=0;i<tbodyData.length;i++){
+    const data=tbodyData[i];
+    console.log(data)
+    trHtml+=`<tr>`
+    //头像
+    trHtml+=`<td>
+    <div class="face">
+        <span class="gender icon-${data.face.gender}"></span>
+        <img src=${data.face.img} alt="">
+    </div>
+</td>`
+//名字
+trHtml+=`<td>
+<div class="people-name">
+    <h4 class="name">${data.name.truename}</h4>
+    <span class="nickname option-05 fs-12">昵称：${data.name.nickname}</span>
+</div>
+</td>`
+//性别
+trHtml+=` <td>
+<div class="gender-wrap">
+    <span class="gender icon-girl ${data.face.gender==="girl"?'': "option-05"}"></span>
+    <span class="gender icon-boy ${data.face.gender==="boy"?'': "option-05"}"></span>
+</div>
+</td>`
+//年龄
+trHtml+=` <td>
+<div class="age text-center">
+    <p>${data.age.number}</p>
+    <span class="option-05 fs-12">（单身狗）</span>
+</div>
+</td>`
+//手机号
+trHtml+=`<td>
+<div class="phone">
+    ${data.phone.code}<span class="option-05">（中国）</span><br />
+    ${data.phone.number}
+</div>
+</td>`
+//国籍
+trHtml+=`<td>
+<div>
+    <img src=${data.country.img} alt=${data.country.name}>
+    <p class="country-name" style="color: ${data.country.color};">${data.country.name}</p>
+</div>
+</td>`
+//爱好
+let likeHtml=``
+data.like.forEach((item)=>{
+    likeHtml+=`<span style="background-color:${item.color};">${item.tag}</span>`
+})
+trHtml+=`<td>
+<div class="likes">
+    ${likeHtml}
+</div>
+</td>`
+//头衔
+trHtml+=` <td>
+<div class="grade">
+    <span class="role-name">${data.rank.name}</span>
+    <div class="grade-wrap icon-grade">
+    <div class="grade-high icon-grade" style="width: ${data.rank.level*16.5}%;"></div>
+    </div>
+</div>
+</td>`
+//设置
+trHtml+=`<td>
+<div class="operation">
+    <a href="javascript: void(0);" class="operation-btn">设置</a>
+    <ul class="links">
+        <a href="javascript: void(0);">编辑</a>
+        <a href="javascript: void(0);">删除</a>
+        <a href="javascript: void(0);">锁定</a>
+        <a href="javascript: void(0);">收起</a>
+    </ul>
+</div>
+</td>`
+    trHtml+=`</tr>`
+}
+tableDom.innerHTML = tableHtml+trHtml ;//元素填内容
--- curriculum/js/tableData.js
+++ curriculum/js/tableData.js
@@ -18,6 +18,33 @@ const jsonTh=[
 const tbodyData=[{
     face:{
         gender:'boy',
-        img:''
+        img:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583671638490&di=957e858d102bae600e6b5472843bdfe6&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201802%2F20%2F20180220165946_RiGPS.thumb.700_0.jpeg'
+    },
+    name:{
+        truename:'小龙女',
+        nickname:'夏末的晨曦'
+    },
+    age:{
+        number:25
+    },
+    phone:{
+        code:'+86',
+        number:1358888888
+    },
+    country:{
+        National_flag:"http://www.web-jshtml.cn/curriculum/javascript/images/china.jpg",
+        name:'中国',
+        color:'green'
+    },
+    like:[
+     {tag:'跑步',color:'#468d9c'},
+     {tag:'看电影',color:'#469c4e'},
+     {tag:'二次元',color:'#9c468b'},
+     {tag:'小猫小狗',color:'#9c4646'},
+     {tag:'小说',color:'#b9af10'}
+    ],
+    rank:{
+        name:'小白',
+        level:2
     }
 }];
